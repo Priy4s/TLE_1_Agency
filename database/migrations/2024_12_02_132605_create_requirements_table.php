49bfd7b2f9ca56c81ccf158331a71a4ead95f46a
Maurice <59695433+mauricevf@users.noreply.github.com>
2024-12-03 10:16:40 +0100

    added login and tailwind

---
+++ database/migrations/2024_12_02_132605_create_requirements_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('requirements', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('job_listing_id')->constrained('job_listings');
+            $table->boolean('drivers_license');
+            $table->boolean('walking');
+            $table->boolean('hands');
+            $table->boolean('standing');
+            $table->boolean('talking');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('requirements');
+    }
+};
