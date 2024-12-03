3c6f185bc3c0f703903f4c97c7108041b61ff33a
Maurice <59695433+mauricevf@users.noreply.github.com>
2024-12-03 12:47:52 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
-
-Route::resource('job_listings', App\Http\Controllers\job_listingController::class)->only('index');
+Route::resource('job_listings', App\Http\Controllers\job_listingController::class)->only(['index', 'create']);
