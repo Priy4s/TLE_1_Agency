973ad109606fa7822c3c47d879f1d43d29de4014
meya <milanela.milanova@gmail.com>
2024-12-17 15:11:11 +0100

    Added translation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,3 +13,8 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body>
+        <x-language-element></x-language-element>
+    </body>
+</html>
